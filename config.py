c06382422ee6487b9df4ddebe569f522dd2be67d
Gael <gael.vanderlee@gmail.com>
2020-03-16 18:14:55 -0700

    Added basic functionality

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import datetime
+from pathlib import Path
 
 image = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -9,3 +10,5 @@ image = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
 start_date = datetime.date(2020, 3, 16)
+file_path = Path(__file__).resolve().parent / "poem.txt"
+assert file_path.exists()
--- pusher.py
+++ pusher.py
@@ -1,4 +1,31 @@
-def get_new_line():
-    return "Temporary"
+import config
+import secret_stuff
+import subprocess as sp
 
-def
+def get_new_line():
+    return "Temporary\n"
+
+
+def write_to_file(text):
+    with open(config.file_path, "a+") as file:
+        file.write(text)
+
+
+def get_text():
+    with open(config.file_path, "r") as file:
+        return file.read()
+
+
+def write_new_line():
+    new_line = get_new_line()
+    print(f"Adding to the poem :\n{new_line}")
+    write_to_file(new_line)
+    print(f"Poem is now:\n{get_text()}")
+
+
+def push_text():
+    sp.run("")
+
+if __name__=="__main__":
+    write_new_line()
+    push_text()
