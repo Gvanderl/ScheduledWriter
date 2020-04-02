50c94618ae97a7699e3b7eb93d683d3f2387d915
Gael <gael.vanderlee@gmail.com>
2020-04-01 22:37:42 -0700

    All done now

--- run.py
+++ run.py
@@ -1,5 +1,12 @@
 import pusher
+from time import sleep
 
-for i in range(pusher.get_pushes_for_day()):
+num_pushes = pusher.get_pushes_for_day()
+for i in range(num_pushes):
+    print(f"Push {i + 1}/{num_pushes}")
     pusher.write_new_line()
     pusher.push_text()
+    if i < (num_pushes - 1):
+        print("Waiting...")
+        sleep(60)
+    print("All done!")
