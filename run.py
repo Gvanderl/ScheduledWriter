b3ea172f2dcbead0295e35c0a8ad07402b4e225e
Gael <gael.vanderlee@gmail.com>
2020-04-11 19:25:24 -0700

    Added prints

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 import pusher
 from time import sleep
+import datetime
 
 num_pushes = pusher.get_pushes_for_day()
+print(f"\n\nGot {num_pushes} for {datetime.datetime.today():%Y-%m-%d}\n")
 for i in range(num_pushes):
     print(f"Push {i + 1}/{num_pushes}")
     pusher.write_new_line()
